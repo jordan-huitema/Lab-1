72ffe3e856ff218553637d36f8a8f24d3d2d4bab
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-03 22:00:13 +1300

    added index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//Lab 1 Jordan Huitema
+//declaring and setting num variables
+var numOne = 50;
+var numTwo = 23;
+//declaring and setting equation variables
+var sum = numOne + numTwo;
+var sub = numOne - numTwo;
+var div = numOne / numTwo;
+var mul = numOne * numTwo;
+//printing Input numbers to console
+console.log();//spacer to make it look pretty
+console.log("Number Input 1 = "+ numOne);
+console.log("Number Input 2 = "+ numTwo);
+//printing calculations to console
+console.log();//spacer to make it look pretty
+console.log(numOne, " + ", numTwo, " = " + sum);
+console.log(numOne, " - ", numTwo, " = " + sub);
+console.log(numOne, " ÷ ", numTwo, " = " + div);
+console.log(numOne, " x ", numTwo, " = " + mul);
